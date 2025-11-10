138206ae60710b77b6e7fdb02812949ff796e23d
Apple <Apple@MacBook-Air.local>
2025-11-10 21:07:53 +0200

    Done.

--- Cheers.java
+++ Cheers.java
@@ -9,12 +9,12 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
 	    String word = args[0].toUpperCase();
-            String aOrAn = "a";
+            String aOrAn = "a ";
             String condition = "AEFHILMNORSX";
 
             for (int i = 0; i < word.length(); i++) {
                 int matches = 0;
-                
+
                 for (int j = 0; j < condition.length(); j++) {
                         if (word.charAt(i) == condition.charAt(j)) {
                                 matches++;
@@ -24,7 +24,7 @@ public class Cheers {
                 if (matches == 1) {
                         aOrAn = "an";
                 } else {
-                        aOrAn = "a";
+                        aOrAn = "a ";
                 }
                 
                 System.out.println("Give me " + aOrAn + " " + word.charAt(i) + ": " + word.charAt(i) + "!");
